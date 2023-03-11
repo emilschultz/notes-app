406d2377bf5bae893339c5bd992b337a97116dce
emilschultz <emil.schultz@amedia.no>
2023-03-11 10:37:59 +0100

    setup initial functionality, pages and routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Notes from './pages/Notes';
+import CreateNote from './pages/CreateNote';
+import EditNote from './pages/EditNote';
+import dummyNotes from './dummy-notes';
 
-function App() {
+import { useState } from 'react';
+
+const App = () => {
+  const [notes, setNotes] = useState(dummyNotes);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Notes notes={notes} />} />
+        <Route path='/create-note' element={<CreateNote />} />
+        <Route path='/edit-note/:id' element={<EditNote />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
-
+};
 export default App;
